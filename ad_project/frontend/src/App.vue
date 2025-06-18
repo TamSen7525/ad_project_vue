6f2b0de0a2e616ca167c78c65e854a5bad16c8e5
User <user@example.com>
2025-06-18 16:20:18 +0300

    Чистка стартового проекта

--- ad_project/frontend/src/App.vue
+++ ad_project/frontend/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>Очистили стартовый проект</h1>
 </template>
+
+<script>
+export default {
+}
+</script>
 
 <style>
 #app {
