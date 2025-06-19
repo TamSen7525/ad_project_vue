168ad54207210ef92870a0038242f966f2ce240b
User <user@example.com>
2025-06-19 17:00:30 +0300

    Реализована верстка страницы списка объявлений пользователя

--- ad_project/frontend/src/views/Ads/AdListView.vue
+++ ad_project/frontend/src/views/Ads/AdListView.vue
@@ -1,14 +1,67 @@
 <template>
 <v-container>
 <v-row justify="center">
-<h1>Ad List</h1>
+<v-col cols="12" sm="8" lg="6">
+<h1 class="text--secondary mb-3 mt-3">My ads</h1>
+<v-card>
+<v-card v-for="ad in ads" :key="ad.id" class="mb-3" max-width="1000">
+<v-row>
+<v-col xs="4">
+<v-img :src="ad.src" height="175px" cover>
+</v-img>
+</v-col>
+<v-col xs="8">
+<h2 class="text--primary">{{ ad.title }}</h2>
+<p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{ ad.desc }}</p>
+<v-card-actions>
+<v-spacer></v-spacer>
+<v-btn color="primary" variant="text" :to="'/ad/' + ad.id">
+Open
+</v-btn>
+</v-card-actions>
+</v-col>
+</v-row>
+</v-card>
+</v-card>
+</v-col>
 </v-row>
 </v-container>
 </template>
+
 <script>
 export default {
 data () {
 return {
+ads:[
+{
+title:"First",
+desc:"First Desc - это более длинное описание для проверки верстки, которое должно обрезаться в списке и показывать троеточие в конце текста при переполнении",
+promo: true,
+src:"https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+id:"1"
+},
+{
+title:"Second",
+desc:"Second Desc",
+promo: true,
+src:"https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+id:"2"
+},
+{
+title:"Third",
+desc:"Third Desc",
+promo: true,
+src:"https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+id:"3"
+},
+{
+title:"Fourth",
+desc:"Fourth Desc",
+promo: true,
+src:"https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+id:"4"
+}
+]
 }
 }
 }
